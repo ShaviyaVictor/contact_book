a33a49cc748a978ee3eb141444da2d6467c0dfd5
ShaviyaVictor <shaviyavictor@gmail.com>
2021-11-22 07:06:25 +0300

    Create variables that will capture the inputted values for our form

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,10 @@ $(document).ready(function(){
   $("form#new-contact").submit(function(event){
     event.preventDefault();
 
+  //    variables that will capture the inputted values of our form    
+  let inputtedFirstName = $("input#new-first-name").val();
+  let inputtedLastName = $("input#new-last-name").val();
+
 
   });
 });
